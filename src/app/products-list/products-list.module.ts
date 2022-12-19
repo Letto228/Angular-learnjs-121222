468ef0b8f8ec66ee54c161b56320543ceaf37690
Letto228 <learnjs@MacBook-Pro-Egor.local>
2022-12-19 23:48:04 +0300

    feat: lecture 2

---
+++ src/app/products-list/products-list.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsListComponent } from './products-list.component';
+
+@NgModule({
+	declarations: [ProductsListComponent],
+	imports: [CommonModule],
+})
+export class ProductsListModule {}
