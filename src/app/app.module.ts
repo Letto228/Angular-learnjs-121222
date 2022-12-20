c331711f1d54cd525d0a92f83352f6fd531b0b37
Sulakin Vadim <sulakin.vadim@gmail.com>
2022-12-20 19:26:33 +0300

    added feat: lecture 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderModule } from './components/header/header.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  readonly imgSrc = '../../../favicon.ico';
+  // readonly imgSrc = 'https://avatars.mds.yandex.net/i?id=b1ba5d199d0c1598bb5762fc6faf2f5526a0cf1a-5513755-images-thumbs&n=13';
+
+  onLogValue(event: MouseEvent) {
+    // event.stopPropagation();
+    console.log(event);
+  }
+}
