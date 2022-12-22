5fc50dafedaf88905e446cf51585ee54b39735e2
Першин Илья <ipershin@diasoft.ru>
2022-12-22 11:07:13 +0500

    исправление замечаний

--- src/app/products-list/product-card/product-card.component.ts
+++ src/app/products-list/product-card/product-card.component.ts
@@ -7,15 +7,9 @@ import { productMock } from './../../shared/products/product.mock';
 	styleUrls: ['./product-card.component.less'],
 })
 export class ProductCardComponent {
-	readonly id = productMock._id;
-	readonly name = productMock.name;
-	readonly price = productMock.price;
-	readonly rating = productMock.rating;
-	readonly imgSrc = productMock.images.at(0)?.url;
-	// readonly imgSrc = 'https://avatars.mds.yandex.net/i?id=b1ba5d199d0c1598bb5762fc6faf2f5526a0cf1a-5513755-images-thumbs&n=13';
+	readonly productMock = productMock;
 
 	buyProduct(id: string) {
-		// event.stopPropagation();
 		console.log('Вы только что купили: ' + id);
 	}
 }
