dfbde968ef8b22f651721bd55ad8559ab3e14383
Letto228 <learnjs@MacBook-Pro-Egor.local>
2022-12-22 19:43:52 +0300

    feat: sanitize application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.less'],
-	interpolation: ['{{', '}}'],
 })
 export class AppComponent {
 	title = 'Angular-learnjs-121222';
-	// window = window;
-
-	onLogValue(event: Event) {
-		console.log(event);
-
-		return true;
-	}
 }
