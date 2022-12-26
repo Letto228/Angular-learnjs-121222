505b6051a0236e151adae71c3bf1152ac0274486
Jenny B <jenia.bykova@gmail.com>
2022-12-26 15:33:22 -0800

    Implement value passing to the rating component

--- src/app/components/rating/rating.module.ts
+++ src/app/components/rating/rating.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {RatingComponent} from "./rating.component";
 import {MatIconModule} from "@angular/material/icon";
+import {MatGridListModule} from "@angular/material/grid-list";
 
 @NgModule({
 	declarations: [RatingComponent],
-	imports: [CommonModule, MatIconModule],
+	imports: [CommonModule, MatIconModule, MatGridListModule],
 	exports: [RatingComponent],
 })
 export class RatingModule {}
--- src/app/model/mocks/mock-product.ts
+++ src/app/model/mocks/mock-product.ts
@@ -8,6 +8,6 @@ export const products: Product[] = [
 		image: "https://c.dns-shop.ru/thumb/st1/fit/0/0/dff090e667cd39dd397678905df52de5/6c3e7aab802deb19513c7cf55dd7c527efb9127d8aba2ed90f3d224577137fe2.jpg",
 		name: '9.6" Планшет Dexp Ursus S290 32 ГБ 3G черный',
 		price: 86,
-		rating: 5,
+		rating: 4,
 	},
 ];
