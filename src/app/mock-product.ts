7b42bfb778c901ce0b89fe3d6bd8153d77a81c50
Jenny B <jenia.bykova@gmail.com>
2022-12-26 12:26:25 -0800

    Add mocks for card

---
+++ src/app/mock-product.ts
@@ -0,0 +1,13 @@
+import {Product} from "./Product";
+
+export const products: Product[] = [
+	{
+		id: "1",
+		feedbacksCount: 5,
+		subCategory: "",
+		image: "https://c.dns-shop.ru/thumb/st1/fit/0/0/dff090e667cd39dd397678905df52de5/6c3e7aab802deb19513c7cf55dd7c527efb9127d8aba2ed90f3d224577137fe2.jpg",
+		name: '9.6" Планшет Dexp Ursus S290 32 ГБ 3G черный',
+		price: 86,
+		rating: 5,
+	},
+];
