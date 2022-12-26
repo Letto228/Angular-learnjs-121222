fa8399aec908493ae55bb9527134bff1849c1be2
Jenny B <jenia.bykova@gmail.com>
2022-12-26 12:25:52 -0800

    Add card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import {BrowserModule} from "@angular/platform-browser";
 
 import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from "./app.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HeaderModule} from "./components/header/header.module";
+import {CardModule} from "./components/card/card.module";
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule, CardModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
