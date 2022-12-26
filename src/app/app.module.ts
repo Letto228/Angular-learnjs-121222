e9d52b026298d162ce33198ee2a708b54f41735a
Jenny B <jenia.bykova@gmail.com>
2022-12-26 12:28:04 -0800

    Add mocks for card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import {BrowserModule} from "@angular/platform-browser";
 import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from "./app.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {HeaderModule} from "./components/header/header.module";
 import {CardModule} from "./components/card/card.module";
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule, CardModule],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CardModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
