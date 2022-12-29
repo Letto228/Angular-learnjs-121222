7fbdf6307f769feb1258b009cf550c74d1cd94d9
Першин Илья <ipershin@diasoft.ru>
2022-12-29 16:18:52 +0500

    'lesson-4'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { applicationConfigMock } from './shared/application-config/application-config.mock';
+import { PopupComponent } from './shared/popup/popup.component';
 
 @Component({
 	selector: 'app-root',
@@ -14,4 +15,25 @@ export class AppComponent {
 	// onMenuClick() {
 	// 	this.isSidenavOpened = !this.isSidenavOpened;
 	// }
+
+	@ViewChild('popup')
+	private popup: PopupComponent | undefined;
+
+	@ViewChild('popupTextInput', {read: ElementRef, static: false})
+	private popupTextInput: ElementRef | undefined;
+
+	@ViewChild('popupText', {read: ElementRef, static: false})
+	private popupText: ElementRef | undefined;
+	
+	openPopup() {
+		if (!this.popup?.visible) {
+			let text: String = this.popupTextInput?.nativeElement.value
+			if (this.popupText && text != '') {
+				this.popupText.nativeElement.innerHTML = this.popupTextInput?.nativeElement.value;
+				this.popup?.toggle();
+			}
+		} else {
+			this.popup?.toggle();
+		}
+	}
 }
---
+++ src/app/shared/popup/popup.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+
+@Component({
+  selector: 'app-popup',
+  templateUrl: './popup.component.html',
+  styleUrls: ['./popup.component.less']
+})
+export class PopupComponent {
+  @Input() set popupTemplate(template: TemplateRef<any>) {
+    this.viewContainer.clear();
+    this.viewContainer.createEmbeddedView(template);
+  }
+  @ViewChild('viewContainer', { read: ViewContainerRef, static: true })
+  private viewContainer!: ViewContainerRef;
+
+  public visible = false;
+
+  toggle() {
+    this.visible = !this.visible;
+  }
+}
