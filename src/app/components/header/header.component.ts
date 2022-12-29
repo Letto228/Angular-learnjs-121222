62179b521992189ae5dd3cef9dfdcf5aef1b0255
Letto228 <learnjs@MacBook-Pro-Egor.local>
2022-12-29 18:08:49 +0300

    feat: pre lecture 5 - sanitize

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,82 +1,17 @@
-import {
-	AfterContentChecked,
-	AfterContentInit,
-	AfterViewChecked,
-	AfterViewInit,
-	Attribute,
-	Component,
-	DoCheck,
-	EventEmitter,
-	Input,
-	OnChanges,
-	OnInit,
-	Output,
-	SimpleChanges,
-} from '@angular/core';
-import { Subject } from 'rxjs';
+import { Attribute, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IApplicationConfig } from '../../shared/application-config/application-config.interface';
-import { applicationConfigMock } from '../../shared/application-config/application-config.mock';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.less'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 	@Input() applicationConfig: IApplicationConfig | undefined;
-	@Input() name: string | undefined;
 
 	@Output() menuClick = new EventEmitter<Event>();
 
 	onMenuClick(event: Event) {
 		this.menuClick.emit(event);
 	}
-
-	constructor(@Attribute('name') private readonly nameAttr: string) {
-		console.log(this.nameAttr);
-	}
-
-	ngOnInit(): void {
-		console.log(this.name);
-	}
-
-	// ngOnChanges({applicationConfig}: SimpleChanges) { // const applicationConfig = {...}.applicationConfig
-	// 	console.log('ngOnChanges');
-	// 	// if (applicationConfig) {
-	// 	// 	console.log('applicationConfig changed', applicationConfig);
-	// 	// }
-
-	// 	// if (applicationConfig && applicationConfig.currentValue?.title !== applicationConfig.previousValue?.title) {
-	// 	// 	console.log('applicationConfig.title changed', applicationConfig);
-	// 	// 	console.log(this.applicationConfig === applicationConfig.currentValue);
-	// 	// }
-	// }
-
-	// onClick() {
-	// 	console.log('click');
-	// }
-
-	// ngOnInit() {
-	// 	console.log('ngOnInit');
-	// }
-
-	// ngDoCheck(): void {
-	// 	console.log('ngDoCheck');
-	// }
-
-	// ngAfterContentInit(): void {
-	// 	console.log('ngAfterContentInit');
-	// }
-
-	// ngAfterContentChecked(): void {
-	// 	console.log('ngAfterContentChecked');
-	// }
-
-	// ngAfterViewInit(): void {
-	// 	console.log('ngAfterViewInit');
-	// }
-
-	// ngAfterViewChecked(): void {
-	// 	console.log('ngAfterViewChecked');
-	// }
 }
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -22,14 +22,6 @@ import { MatDrawer } from '@angular/material/sidenav';
 	styleUrls: ['./sidenav.component.less'],
 })
 export class SidenavComponent {
-	// @Input() set navigationList(template: TemplateRef<unknown>) {
-	// 	this.viewContainer?.clear();
-
-	// 	// if (template) {
-	// 		this.viewContainer?.createEmbeddedView(template);
-	// 	// }
-	// };
-
 	@ViewChild(MatDrawer, { read: MatDrawer, static: true })
 	private drawer!: MatDrawer;
 	@ViewChild('viewContainer', { read: ViewContainerRef, static: true })
@@ -37,51 +29,12 @@ export class SidenavComponent {
 
 	@ContentChild('navigationList', { static: true })
 	private navigationListTemplate!: TemplateRef<unknown>;
-	// @ContentChild('h', {static: true, descendants: false})
-	// private set h(elementRef: ElementRef | undefined) {
-	// 	console.log(elementRef);
-	// };
-
-	// @ViewChild(MatDrawer, {read: ElementRef, static: false})
-	// private drawerElement: ElementRef<HTMLElement> | undefined;
 
 	ngOnInit() {
 		this.viewContainer.createEmbeddedView(this.navigationListTemplate);
-		// console.log(this.drawer);
-		// console.log(this.drawerElement);
 	}
-
-	// opened = false;
-
-	// @Input() navigationList: TemplateRef<unknown> | undefined;
-	// ngOnChanges({navigationList}: SimpleChanges) {
-	// 	if (navigationList) {
-	// 		this.viewContainer?.clear();
-
-	// 		if (this.navigationList) {
-	// 			this.viewContainer?.createEmbeddedView(this.navigationList);
-	// 		}
-
-	// 	}
-	// }
 
 	sidenavToggle() {
 		this.drawer.toggle();
-		// console.log(this.drawerElement?.nativeElement);
 	}
-
-	// ngOnInit(): void {
-	// 	console.log(this.drawer);
-	// 	this.drawer?.toggle();
-	// }
-
-	// ngAfterViewInit(): void {
-	// 	console.log(this.drawer);
-	// 	// this.opened = true;
-	// 	// this.drawer?.toggle();
-	// }
-
-	// ngOnDestroy(): void {
-
-	// }
 }
