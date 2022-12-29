18772a62d7cfc7a9f586db585d5ad71db4d86ee4
Valentin Yushkevich <valiantsinyushkevich@gmail.com>
2022-12-29 16:16:13 +0300

    feat: change hooks

--- src/app/shared/popup/popup/popup-contaner/popup-contaner.component.ts
+++ src/app/shared/popup/popup/popup-contaner/popup-contaner.component.ts
@@ -1,17 +1,17 @@
-import { Component, Input, TemplateRef, ViewChild, ViewContainerRef, OnInit } from '@angular/core';
+import { Component, Input, TemplateRef, ViewChild, ViewContainerRef, OnChanges } from '@angular/core';
 
 @Component({
 	selector: 'app-popup-contaner',
 	templateUrl: './popup-contaner.component.html',
 	styleUrls: ['./popup-contaner.component.less'],
 })
-export class PopupContanerComponent implements OnInit {
+export class PopupContanerComponent implements OnChanges {
 	@Input() popupTemplate: TemplateRef<unknown> | undefined;
 
 	@ViewChild('popupContainer', { read: ViewContainerRef, static: true })
 	private popupContainer!: ViewContainerRef;
 
-	ngOnInit() {
+	ngOnChanges() {
 		this.popupContainer.clear();
 		if (this.popupTemplate) {
 			this.popupContainer.createEmbeddedView(this.popupTemplate);
