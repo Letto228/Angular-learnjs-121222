de8aabbb9ad2b9f50e79533ba4c0e77d50c68332
mrfrac <fracturer@gmail.com>
2023-01-03 17:00:20 +0500

    feat: if template undefined

--- src/app/shared/popup/popup-host/popup-host.component.ts
+++ src/app/shared/popup/popup-host/popup-host.component.ts
@@ -6,8 +6,10 @@ import { Component, Input, TemplateRef, ViewChild, ViewContainerRef } from '@ang
 })
 export class PopupHostComponent {
 	@Input() set popupTemplate(template: TemplateRef<unknown>) {
-		this.viewContainer.clear();
-		this.viewContainer.createEmbeddedView(template);
+		if (template !== undefined) {
+			this.viewContainer.clear();
+			this.viewContainer.createEmbeddedView(template);
+		}
 	}
 	@ViewChild('viewContainer', { read: ViewContainerRef, static: true })
 	private viewContainer!: ViewContainerRef;
