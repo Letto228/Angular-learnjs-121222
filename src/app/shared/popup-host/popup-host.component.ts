d1abd935b53773f0a59764038f11d060776fee6e
Sergey Gonchar <dev.sergey.gonchar@gmail.com>
2023-01-03 09:16:22 +0300

    feat: refactoring

--- src/app/shared/popup-host/popup-host.component.ts
+++ src/app/shared/popup-host/popup-host.component.ts
@@ -1,22 +1,49 @@
-import { Component, HostBinding, Input, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import {
+	Component,
+	HostBinding,
+	Input,
+	OnChanges,
+	SimpleChanges,
+	TemplateRef,
+	ViewChild,
+	ViewContainerRef,
+} from '@angular/core';
 
 @Component({
 	selector: 'app-popup-host',
 	templateUrl: './popup-host.component.html',
 	styleUrls: ['./popup-host.component.less'],
 })
-export class PopupHostComponent {
-	@Input() set popupTemplate(template: TemplateRef<unknown>) {
-		this.viewContainer?.clear();
-		this.viewContainer?.createEmbeddedView(template);
-	}
+export class PopupHostComponent implements OnChanges {
+	@Input() popupTemplate: TemplateRef<unknown> | undefined;
 
 	@HostBinding('class.opened') private opened = false;
 
 	@ViewChild('viewContainer', { read: ViewContainerRef, static: true })
 	private viewContainer!: ViewContainerRef;
 
+	ngOnChanges({ popupTemplate }: SimpleChanges) {
+		if (popupTemplate) {
+			this.updateView();
+		}
+	}
+
 	public toggleOpened() {
 		this.opened = !this.opened;
+		this.updateView();
+	}
+
+	private updateView() {
+		this.opened ? this.open() : this.viewContainer?.clear();
+	}
+
+	private open() {
+		if (this.viewContainer?.length) {
+			this.viewContainer?.clear();
+		}
+
+		if (this.popupTemplate) {
+			this.viewContainer?.createEmbeddedView(this.popupTemplate);
+		}
 	}
 }
