82d7e275cede13d8181f52ad9f3bae0a66d233b0
Jenny B <jenia.bykova@gmail.com>
2023-01-04 12:52:48 -0800

    Refactoring imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from "./app.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {CardModule} from "./components/card/card.module";
-import {MatIconModule} from "@angular/material/icon";
-import {RatingModule} from "./components/rating/rating.module";
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CardModule, MatIconModule, RatingModule],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CardModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
