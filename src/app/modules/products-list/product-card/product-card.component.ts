20e650c2a23edce9b4033df5dc23dabd254cf0b8
atuzov <atuzov@diasoft.ru>
2023-01-08 19:42:19 +0300

    feat: lecture-3

--- src/app/modules/products-list/product-card/product-card.component.ts
+++ src/app/modules/products-list/product-card/product-card.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { productMock } from '../../../shared/products/product.mock';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IProduct } from '../../../shared/products/product.interface';
 
 @Component({
 	selector: 'app-product-card',
@@ -7,15 +7,24 @@ import { productMock } from '../../../shared/products/product.mock';
 	styleUrls: ['./product-card.component.less'],
 })
 export class ProductCardComponent {
-	readonly product = productMock;
+	@Input() product: IProduct | undefined;
+
+	@Output() productBuyClick = new EventEmitter<Event>();
 
 	onProductBuy(event: Event) {
+		this.productBuyClick.emit(event);
 		event.stopPropagation();
-
 		console.log('Buy');
 	}
 
 	isStarActive(starIndex: number): boolean {
 		return Boolean(this.product && this.product.rating >= starIndex);
+	}
+
+	getImageUrl(): string {
+		if (this.product) {
+			return this.product?.images[0].url;
+		}
+		return '';
 	}
 }
--- src/app/modules/products-list/products-list.component.ts
+++ src/app/modules/products-list/products-list.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { productMock } from '../../shared/products/product.mock';
 
 @Component({
 	selector: 'app-products-list',
 	templateUrl: './products-list.component.html',
 	styleUrls: ['./products-list.component.less'],
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+	readonly product = productMock;
+
+	productBuyClick(event: Event) {
+		console.log(event);
+	}
+}
