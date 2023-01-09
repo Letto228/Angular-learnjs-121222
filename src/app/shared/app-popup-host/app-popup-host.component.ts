8bb420b468c19425cad79b4012f7a11f50f2a769
atuzov <atuzov@diasoft.ru>
2023-01-09 19:38:01 +0300

    feat: lecture 4

---
+++ src/app/shared/app-popup-host/app-popup-host.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+
+@Component({
+	selector: 'app-popup-host',
+	templateUrl: './app-popup-host.component.html',
+	styleUrls: ['./app-popup-host.component.less'],
+})
+export class AppPopupHostComponent {
+	@ViewChild('viewContainer', { read: ViewContainerRef, static: true })
+	private viewContainer!: ViewContainerRef;
+
+	@Input() set popupTemplate(template: TemplateRef<unknown>) {
+		this.viewContainer?.clear();
+
+		if (template) {
+			this.viewContainer?.createEmbeddedView(template);
+		}
+	}
+}
