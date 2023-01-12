57ef887ec2239c61dbf7147c51c05058045b72fd
e.g.sidorov <e.g.sidorov@tinkoff.ru>
2023-01-12 18:20:54 +0300

    feat: lecture 6 decision

--- src/app/modules/products-list/products-list.module.ts
+++ src/app/modules/products-list/products-list.module.ts
@@ -3,10 +3,20 @@ import { CommonModule } from '@angular/common';
 import { ProductsListComponent } from './products-list.component';
 import { ProductCardModule } from './product-card/product-card.module';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { PaginationModule } from '../../shared/pagination/pagination.module';
 
 @NgModule({
 	declarations: [ProductsListComponent],
-	imports: [CommonModule, ProductCardModule, MatProgressSpinnerModule],
+	imports: [
+		CommonModule,
+		ProductCardModule,
+		MatProgressSpinnerModule,
+		MatIconModule,
+		MatButtonModule,
+		PaginationModule,
+	],
 	exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
