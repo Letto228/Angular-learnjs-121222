cf559204668cd42a82567252361297dedd91dfc0
Александра Лушкина <aleksandra-lushkina@yandex.ru>
2023-01-20 18:19:23 +0300

    feat: add card component

---
+++ src/app/products-list/product-card/product-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { productMock } from '../../shared/products/product.mock';
+
+@Component({
+	selector: 'app-product-card',
+	templateUrl: './product-card.component.html',
+	styleUrls: ['./product-card.component.less'],
+})
+export class ProductCardComponent {
+	readonly product = productMock;
+}
--- src/app/products-list/products-list.module.ts
+++ src/app/products-list/products-list.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsListComponent } from './products-list.component';
+import { ProductCardModule } from './product-card/product-card.module';
 
 @NgModule({
 	declarations: [ProductsListComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, ProductCardModule],
+	exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
