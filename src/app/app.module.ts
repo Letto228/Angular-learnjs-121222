74a502afe2acea30e4c41b2b8af6c3600b5e340c
ibiushkin <ibiushkin@kept.ru>
2023-01-22 20:24:11 +0300

    feat: add popup-host component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { HeaderModule } from './components/header/header.module';
 import { ProductsListModule } from './modules/products-list/products-list.module';
 import { SidenavModule } from './components/sidenav/sidenav.module';
 import { MatListModule } from '@angular/material/list';
+import { PopupHostComponent } from './shared/popup-host/popup-host.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, PopupHostComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
---
+++ src/app/shared/popup-host/popup-host.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+
+@Component({
+	selector: 'app-popup-host',
+	templateUrl: './popup-host.component.html',
+	styleUrls: ['./popup-host.component.less'],
+})
+export class PopupHostComponent {
+	@Input() set popupTemplate(template: TemplateRef<unknown>) {
+		this.popupContainer?.clear();
+
+		if (template && this.popupContainer) {
+			this.popupContainer.createEmbeddedView(template);
+		}
+	}
+
+	@ViewChild('popupContainer', { read: ViewContainerRef, static: true })
+	private popupContainer: ViewContainerRef | undefined;
+}
