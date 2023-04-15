c0b32668da1f119b586557cd4b1ed2c3339e4eb8
dkruglova <dkruglova@diasoft.ru>
2023-04-15 20:59:49 +0300

    fix: modify popup-host

--- src/app/shared/popup/popup-host/popup-host.component.ts
+++ src/app/shared/popup/popup-host/popup-host.component.ts
@@ -1,11 +1,21 @@
-import { Component, Input, OnChanges, SimpleChanges, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import {
+	AfterViewInit,
+	Component,
+	HostBinding,
+	Input,
+	OnChanges,
+	SimpleChanges,
+	TemplateRef,
+	ViewChild,
+	ViewContainerRef,
+} from '@angular/core';
 
 @Component({
 	selector: 'app-popup-host',
 	templateUrl: './popup-host.component.html',
 	styleUrls: ['./popup-host.component.less'],
 })
-export class PopupHostComponent implements OnChanges {
+export class PopupHostComponent implements OnChanges, AfterViewInit {
 	/*@Input() set popupTemplate(template: TemplateRef<unknown> | undefined) {
 		this.popupContainer?.clear();
 
@@ -18,11 +28,32 @@ export class PopupHostComponent implements OnChanges {
 	@ViewChild('popupContainer', { read: ViewContainerRef, static: true })
 	private popupContainer: ViewContainerRef | undefined;
 
-	ngOnChanges({ popupTemplate }: SimpleChanges) {
-		this.popupContainer?.clear();
+	private hostDisplay: string | undefined = 'none';
+	@HostBinding('style.display') get getHostDisplay() {
+		return this.hostDisplay;
+	}
 
+	// @HostBinding('style')
+	// private hostStyle: string = 'display: none';
+
+	ngOnChanges({ popupTemplate }: SimpleChanges) {
 		if (popupTemplate && this.popupTemplate) {
 			this.popupContainer?.createEmbeddedView(this.popupTemplate);
 		}
+		this.hostDisplay = this.popupContainer?.length !== 0 ? 'block' : 'none';
+
+		// const popupStyle = 'display: block;position: absolute;left: 0;top: 0;width: 80%;height: 80%;text-align: center;z-index: 1000;margin: 10%;background: white;';
+		// this.hostStyle = this.popupContainer?.length !== 0 ? popupStyle : 'display: none';
+	}
+
+	ngAfterViewInit() {
+		// Решение для @ViewChild со свойством static: false
+		/*this.popupContainer?.clear();
+		if (this.popupTemplate) {
+			this.popupContainer?.createEmbeddedView(this.popupTemplate);
+		}
+		setTimeout(() => {
+			this.hostDisplay = this.popupContainer?.length !== 0 ? 'block' : 'none';
+		}, 50);*/
 	}
 }
